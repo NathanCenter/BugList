a96066a8de017805f0620b343ca7cca45b078a3a
Nathan Center <nathanjcenter@gmail.com>
2022-08-12 12:41:10 -0500

    added dynamic dropdown to the bug list and the project list

--- bugList/bugList/Startup.cs
+++ bugList/bugList/Startup.cs
@@ -34,6 +34,7 @@ namespace bugList
            
             services.AddTransient<IProjectListRepository, ProjectListRepository>();
             services.AddTransient<IBugRepository, BugRepository>();
+            services.AddTransient<IBugTypeRepository, BugTypeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
